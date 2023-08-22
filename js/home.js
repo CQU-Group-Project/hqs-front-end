0bf043640f956132ab210f133d6cdbc3d5561b92
deependra58 <karkideependra58@gmail.com>
2023-08-22 22:21:41 +1000

    Project initialized with few customer pages

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//     // Initialize Datepicker
+//     $('#checkinDate').datepicker({
+//         format: 'yyyy-mm-dd', // Set your desired date format
+//         autoclose: true // Close the Datepicker when a date is selected
+//     });
+
+//     // Open Datepicker when clicking on the input field
+//     $('#checkinDate').on('click', function () {
+//         $(this).datepicker('show');
+//     });
+// });
